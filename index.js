add1abbaea97bd4a553e0f3c25ceb693eb083aef
Revan Sarbast Salih <revansarbast8@gmail.com>
2022-03-04 12:22:56 +0300

    parsing the url and geting the method

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer(function (req, res) {
+  //parse the url
+  const path = url.parse(req.url, true);
+  //remove the first and last slashes
+  const trimmedPath = path.pathname.replace(/^\/+|\/+$/g, "");
+  //get the method
+  const method = req.method.toLocaleLowerCase();
+
+  //respond the the request
   res.end("Hello World\n");
+
+  console.log("Requesting on :", trimmedPath, "and the method is :", method);
 });
 
 server.listen(3000, function () {
